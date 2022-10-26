c172c8f7d6c35001211dfb8f01101c61e9981ba4
Nikolay <79745981+astafyevnu@users.noreply.github.com>
2022-10-26 21:58:09 +0800

    Update task2.py

--- seminar4/hw/task2.py
+++ seminar4/hw/task2.py
@@ -1,15 +1,19 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+
 n = int(input("input number: "))
 
-lst = []
+# n = 2310
 
+lst = []
+num = n
 i = 2
 
 while i * i <= n:
-    while n % i == 0:
-        n //= i
+    while num % i == 0:
+        num //= i
         lst.append(i)
+        
     i += 1
 
 print(f"Cписок простых множителей числа: {set(lst)}" if len(
