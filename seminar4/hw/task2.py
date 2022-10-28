4e5fbed861dc58c6a4e1407735e7989a59ecca0c
Nikolay <n.astafyev@yandex.ru>
2022-10-28 18:18:28 +0800

    save

--- seminar4/hw/task2.py
+++ seminar4/hw/task2.py
@@ -2,15 +2,17 @@
 
 n = int(input("input number: "))
 
-lst = []
+# n = 2310
 
+lst = []
+num = n
 i = 2
 
 while i * i <= n:
-    while n % i == 0:
-        n //= i
+    while num % i == 0:
+        num //= i
         lst.append(i)
+        
     i += 1
 
-print(f"Cписок простых множителей числа: {set(lst)}" if len(
-    lst) > 1 else "Число делится само на себя.")
+print(f"Cписок простых множителей числа: {set(lst)}")
