401c1b2135348ad8011c722e2ab9fa0bed72f248
Nikolay <n.astafyev@yandex.ru>
2022-12-15 21:52:09 +0800

    add tb

---
+++ seminar9/first_tgbot/main.py
@@ -0,0 +1,47 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('5669628428:AAFrtE5LgdXmFxZuYXAkHRpMLxCdBbafzeo')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
+
+# @bot.message_handler(content_types=['text'])
+# def get_user_text(message):
+#     if message.text == 'Hello':
+#         bot.send_message(message.chat.id, 'Сам Hello!', parse_mode='html')
+#     elif message.text == 'id':
+#         bot.send_message(message.chat.id, f'Твой ID: {message.from_user.id}', parse_mode='html')
+#     elif message.text == 'photo':
+#         photo = open(r'D:\2qr\Py\Py_seminars\seminar9\first_tgbot\python.jpg', 'rb')
+#         bot.send_photo(message.chat.id, photo)
+#     else:
+#         bot.send_message(message.chat.id, 'Я тебя не понимаю!', parse_mode='html')
+
+
+@bot.message_handler(content_types=['photo'])
+def get_user_photo(message):
+    bot.send_message(message.chat.id, 'Ого! Картинка!')
+
+
+@bot.message_handler(commands=['website'])
+def website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Посетить вебсайт', url='https://github.com'))
+    bot.send_message(message.chat.id, 'Welcome', reply_markup=markup)
+
+
+@bot.message_handler(commands=['help'])
+def website(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    website = types.KeyboardButton('Website')
+    start = types.KeyboardButton('Start')
+    markup.add(website, start)
+    bot.send_message(message.chat.id, 'Welcome', reply_markup=markup)
+
+
+bot.polling(none_stop=True)
