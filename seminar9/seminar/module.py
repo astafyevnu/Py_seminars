7e8414cec82c0fad1556a57ded6b2272a6a6a512
Nikolay <n.astafyev@yandex.ru>
2022-12-16 02:39:47 +0800

    del

--- seminar9/seminar/module.py
+++
@@ -1,5 +0,0 @@
-spis = ['дерево', 'мир', 'земля', 'губернаторство', 'убрава',
-'жеребьёвка', 'заботливость', 'меблировка', 'набивка', 'рабство']
-inp = set(input('Введите строку с символами: ').lower())
-print(spis, [new_spis for new_spis in spis if not inp.issubset(
-set(new_spis.lower()))], sep='\n')
